20215c6d6d362c6ec500d53905422f648e0a9637
SOtalvaroO <santiago.otalvaroo@udea.edu.co>
2022-03-26 10:46:45 -0500

    random

--- code_breaker.js
+++ code_breaker.js
@@ -1,4 +1,17 @@
-const secret = "1234";
+let secret = "";
+
+var arr = [];
+while(arr.length < 4){
+    var r = Math.floor(Math.random() * 10);
+    if(arr.indexOf(r) === -1) arr.push(r);
+}
+
+arr.forEach(element => {
+  secret = secret + element;
+});
+
+console.log(secret)
+
 
 function solve(a) {
   let result = "";

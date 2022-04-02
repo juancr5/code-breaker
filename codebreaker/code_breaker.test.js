2d47c17042fc4573f62e67fe5cdb408fd16065f0
SOtalvaroO <santiago.otalvaroo@udea.edu.co>
2022-04-02 11:37:13 -0500

    code breaker changes added

--- codebreaker/code_breaker.test.js
+++ codebreaker/code_breaker.test.js
@@ -1,44 +1,44 @@
 
-// const codeBreaker = require('./code_breaker')
+const codeBreaker = require('./code_breaker')
 
-// describe("Win case", () => {
-//   test("case XXXX", () => {
-//     let result = codeBreaker.solve("1234");
-//     expect(result).toBe("XXXX");
-//   });
+describe("Win case", () => {
+  test("case XXXX", () => {
+    let result = codeBreaker.solve("1234");
+    expect(result).toBe("XXXX");
+  });
 
-//   test("case XX", () => {
-//     let result = codeBreaker.solve("1298");
-//     expect(result).toBe("XX");
-//   });
+  test("case XX", () => {
+    let result = codeBreaker.solve("1298");
+    expect(result).toBe("XX");
+  });
 
-//   test("case XX-", () => {
-//     let result = codeBreaker.solve("1248");
-//     expect(result).toBe("XX-");
-//   });
-//   test("case XX-", () => {
-//     let result = codeBreaker.solve("1293");
-//     expect(result).toBe("XX-");
-//   });
-//   test("case XX--", () => {
-//     let result = codeBreaker.solve("1243");
-//     expect(result).toBe("XX--");
-//   });
-//   test("case ----", () => {
-//     let result = codeBreaker.solve("4321");
-//     expect(result).toBe("----");
-//   });
-//   test("case --", () => {
-//     let result = codeBreaker.solve("4356");
-//     expect(result).toBe("--");
-//   });
-//   test("case XXX", () => {
-//     let result = codeBreaker.solve("5234");
-//     expect(result).toBe("XXX");
-//   });
-//   test("case no asserts", () => {
-//     let result = codeBreaker.solve("9876");
-//     expect(result).toBe("");
-//   });
+  test("case XX-", () => {
+    let result = codeBreaker.solve("1248");
+    expect(result).toBe("XX-");
+  });
+  test("case XX-", () => {
+    let result = codeBreaker.solve("1293");
+    expect(result).toBe("XX-");
+  });
+  test("case XX--", () => {
+    let result = codeBreaker.solve("1243");
+    expect(result).toBe("XX--");
+  });
+  test("case ----", () => {
+    let result = codeBreaker.solve("4321");
+    expect(result).toBe("----");
+  });
+  test("case --", () => {
+    let result = codeBreaker.solve("4356");
+    expect(result).toBe("--");
+  });
+  test("case XXX", () => {
+    let result = codeBreaker.solve("5234");
+    expect(result).toBe("XXX");
+  });
+  test("case no asserts", () => {
+    let result = codeBreaker.solve("9876");
+    expect(result).toBe("");
+  });
 
-// });
+});
